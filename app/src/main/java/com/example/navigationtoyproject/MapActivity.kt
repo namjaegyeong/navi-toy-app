ea47bce9e11f7acbefbfdbb354b83b0338312da0
Nam Jae Gyeong <57249939+namjaegyeong@users.noreply.github.com>
2025-01-10 12:50:22 +0900

    사용자는 on/off 만 실시

--- app/src/main/java/com/example/navigationtoyproject/MapActivity.kt
+++ app/src/main/java/com/example/navigationtoyproject/MapActivity.kt
@@ -19,7 +19,6 @@ import com.example.navigationtoyproject.api.model.ResponseMapVersionDto
 import com.example.navigationtoyproject.databinding.ActivityMapBinding
 import com.example.navigationtoyproject.datastore.DataStoreHelper
 import com.example.navigationtoyproject.datastore.MapDataMapper
-import com.example.navigationtoyproject.datastore.MapDataMapper.protoListToDtoList
 import com.example.navigationtoyproject.datastore.MapDataStoreManager
 import com.example.navigationtoyproject.factory.MapViewModelFactory
 import com.example.navigationtoyproject.repository.UserPreferencesRepository
@@ -37,6 +36,11 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
+/**
+ *  카카오 맵 초기화 후,
+ *  DataStore 에서 가져온 커스텀 마커를 ON/OFF 할 수 있는 Activity
+ */
+
 class MapActivity : AppCompatActivity() {
     companion object {
         private const val ICON_WIDTH = 45
@@ -45,11 +49,14 @@ class MapActivity : AppCompatActivity() {
 
     private var mBinding: ActivityMapBinding? = null
     private val binding get() = mBinding!!
-    private val markerMap = mutableMapOf<Int, KNMapMarker>()
     private var resizedIcon: Bitmap? = null
 
-    private var mapViewModel: MapViewModel? = null
-    private var userPreferencesRepository: UserPreferencesRepository? = null
+    private lateinit var mapViewModel: MapViewModel
+    private lateinit var userPreferencesRepository: UserPreferencesRepository
+
+    private var markersVisible: Boolean = true
+
+    private var markers: List<MapData> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,20 +70,30 @@ class MapActivity : AppCompatActivity() {
             insets
         }
 
+        // 마커 아이콘 크기 조절
         val originalIcon = BitmapFactory.decodeResource(resources, R.drawable.signal_light)
         resizedIcon = resizeBitmap(originalIcon!!, ICON_WIDTH, ICON_HEIGHT)
 
         init()
 
+        // 코루틴으로 마커 데이터 처리
         lifecycleScope.launch {
+            // 테스트용 목데이터 생성 (필요시 주석 해제)
+            // makeMockData()
+
+            // 카카오 Map 초기화 완료 대기
             delay(3000)
 
+            // 맵 버전을 체크하는 서비스 종료 이후, 만약 맵 버전이 최신화되지 않았다면(requestFlag == 1), 최신화된 주제도를 요청
+            // 맵 버전이 최신화됐다면(requestFlag == 0), 저장된 주제도를 표출
             checkAndHandleRequestFlag()
 
+            // 현재 저장된 맵 버전 수집
             val mapVersion = DataStoreHelper.getMapVersion(applicationContext).first() ?: "0.0.0"
             binding.mapVersionTextView.text = "Map Version: $mapVersion"
 
-            mapViewModel?.mapDataList?.collectLatest { result ->
+            // ViewModel 에서 커스텀 마커 데이터 수집
+            mapViewModel.mapDataList.collectLatest { result ->
                 when (result) {
                     is NetworkResult.Loading -> {
                         binding.progressBar.visibility = View.VISIBLE
@@ -84,12 +101,12 @@ class MapActivity : AppCompatActivity() {
                     is NetworkResult.Success -> {
                         binding.progressBar.visibility = View.GONE
 
-                        val markers = result.data?.mapDataList
-                        if(markers != null) {
-                            MapDataStoreManager.setMapDataList(this@MapActivity, MapDataMapper.dtoListToProtoList(markers))
-                            markers.forEach { marker ->
-                                binding.mapView.addCustomMarker(marker)
-                            }
+                        val mapDataList = result.data?.mapDataList!!
+                        MapDataStoreManager.setMapDataList(this@MapActivity, MapDataMapper.dtoListToProtoList(mapDataList))
+                        markers = MapDataStoreManager.getMapDataList(this@MapActivity).first()?.mapDataListList ?: listOf()
+
+                        markers.forEach { marker ->
+                            binding.mapView.addCustomMarker(marker)
                         }
                     }
                     is NetworkResult.Error -> {
@@ -102,7 +119,7 @@ class MapActivity : AppCompatActivity() {
             }
         }
 
-        // Start the TerminationService
+        // TerminationService 시작
         val serviceIntent = Intent(this, TerminationService::class.java)
         startService(serviceIntent)
     }
@@ -114,10 +131,22 @@ class MapActivity : AppCompatActivity() {
         userPreferencesRepository = UserPreferencesRepository()
 
         // ViewModelFactory 생성
-        val factory = MapViewModelFactory(userPreferencesRepository!!)
+        val factory = MapViewModelFactory(userPreferencesRepository)
 
-        // ViewModelProvider를 통해 ViewModel 인스턴스 얻기
+        // ViewModelProvider를 통해 ViewModel 인스턴스 생성
         mapViewModel = ViewModelProvider(this, factory)[MapViewModel::class.java]
+
+        // 마커 표시 토글 버튼 설정
+        binding.overlayButton.setOnClickListener {
+            if (markersVisible) {
+                removeMarkers()
+                binding.overlayButton.text = "Show Markers"
+            } else {
+                addMarkers()
+                binding.overlayButton.text = "Hide Markers"
+            }
+            markersVisible = !markersVisible
+        }
     }
 
     private fun initMapView(mapView: KNMapView) {
@@ -139,55 +168,57 @@ class MapActivity : AppCompatActivity() {
         }
     }
 
-    // 지도 SDK에 맞게 마커 추가 로직 구현
-    // 커스텀 이미지는 Bitmap으로 변환 후 마커에 설정
-    private fun KNMapView.addCustomMarker(dto: MapDataDto) {
-        val markerId = dto.customLocationMarkerId
-        val xCoordinate = dto.wgs84_x
-        val yCoordinate = dto.wgs84_y
+    // 지도에 커스텀 마커 추가 함수
+    // 커스텀 이미지는 Bitmap으로 변환 후 커스텀 마커 설정
+    private fun KNMapView.addCustomMarker(data: MapData) {
+        val markerId = data.customLocationMarkerId
+        val xCoordinate = data.wgs84X
+        val yCoordinate = data.wgs84Y
 
-        if (markerMap.containsKey(markerId)) return
-        markerMap[markerId] = KNMapMarker(WGS84ToKATEC(xCoordinate, yCoordinate).toFloatPoint()).apply {
+        KNMapMarker(WGS84ToKATEC(xCoordinate, yCoordinate).toFloatPoint()).apply {
             icon = resizedIcon
             tag = markerId
             this@addCustomMarker.addMarker(this)
         }
     }
 
+    // Bitmap 크기 조절 함수
     private fun resizeBitmap(source: Bitmap, width: Int, height: Int): Bitmap {
         return Bitmap.createScaledBitmap(source, width, height, true)
     }
 
+    // 요청 플래그 확인 및 처리 함수
     private suspend fun checkAndHandleRequestFlag() {
         val requestFlag = DataStoreHelper.getRequestFlag(applicationContext).first() ?: 0
         if (requestFlag == 1) {
             Log.d("MainActivity", "Request flag is set to 1. Performing API request.")
 
-            // Perform the desired API request
-            mapViewModel?.findMapDataList(
+            mapViewModel.findMapDataList(
                 responseMapVersionDto = ResponseMapVersionDto(
                     mapVersion = DataStoreHelper.getMapVersion(applicationContext).first() ?: "0.0.0"
                 )
             )
 
-            // Reset the request flag to 0 after handling
+            // 처리 후 요청 플래그를 0으로 리셋
             DataStoreHelper.resetRequestFlag(applicationContext)
             Log.d("MainActivity", "Request flag reset to 0.")
         } else {
-            val markers = MapDataStoreManager.getMapDataList(this@MapActivity).first()
+            markers =  MapDataStoreManager.getMapDataList(this@MapActivity).first()?.mapDataListList ?: listOf()
 
-            if(markers != null) {
-                protoListToDtoList(markers.mapDataListList).forEach { marker ->
-                    binding.mapView.addCustomMarker(marker)
-                }
+            markers.forEach { marker ->
+                binding.mapView.addCustomMarker(marker)
             }
         }
     }
 
+    // 테스트용 목데이터 생성 및 DataStore에 저장 함수
     private suspend fun makeMockData() {
         DataStoreHelper.setMapVersion(applicationContext, "1.0.2")
 
         val mockList = listOf(
+            MapDataDto(1, 127.451386, 36.631166, "1.0.2"),
+            MapDataDto(2, 127.452995, 36.632604, "1.0.2"),
+            MapDataDto(3, 127.454529, 36.630899, "1.0.2"),
             MapDataDto(4, 127.456138, 36.629987, "1.0.2"),
             MapDataDto(5, 127.455806, 36.628351, "1.0.2"),
             MapDataDto(6, 127.457149, 36.625925, "1.0.2"),
@@ -200,12 +231,26 @@ class MapActivity : AppCompatActivity() {
         MapDataStoreManager.setMapDataList(this@MapActivity, MapDataMapper.dtoListToProtoList(mockList))
     }
 
+    // 마커 추가 함수
+    private fun addMarkers() {
+        markers.forEach { marker ->
+            binding.mapView.addCustomMarker(marker)
+        }
+    }
+
+    // 모든 마커 제거 함수
+    private fun removeMarkers() {
+        binding.mapView.removeMarkersAll()
+    }
+
     override fun onDestroy() {
         mBinding = null
         super.onDestroy()
-        // Bitmap 회수
-        if (!resizedIcon!!.isRecycled) {
-            resizedIcon!!.recycle()
+        // Bitmap 자원 해제
+        resizedIcon?.let {
+            if (!it.isRecycled) {
+                it.recycle()
+            }
         }
     }
 }
